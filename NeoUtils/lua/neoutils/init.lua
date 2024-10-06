347c511f02cfd8fdbd3821c47659375431f5bc8b
Nias26 <genangeliludovico26@gmail.com>
2024-10-06 13:55:24 +0200

    refactor: assigned folder and files for every utility

--- NeoUtils/lua/neoutils/init.lua
+++ NeoUtils/lua/neoutils/init.lua
@@ -7,27 +7,13 @@ local defaults = {
 }
 
 function M.setup(opts)
+	-- Load options
 	opts = opts or defaults
-end
 
-function M.split(direction)
-	require("telescope.builtin").find_files({
-		attach_mappings = function(_, t_map)
-			t_map("n", "<CR>", function(prompt_bufnr)
-				local selected = require("telescope.actions.state").get_selected_entry()
-				local cmd = direction .. " " .. selected[1]
-				require("telescope.actions").close(prompt_bufnr)
-				vim.api.nvim_command(cmd)
-			end)
-			t_map("i", "<CR>", function(prompt_bufnr)
-				local selected = require("telescope.actions.state").get_selected_entry()
-				local cmd = direction .. " " .. selected[1]
-				require("telescope.actions").close(prompt_bufnr)
-				vim.api.nvim_command(cmd)
-			end)
-			return true
-		end,
-	})
+	-- Load utils
+	if opts.enabled.split then
+		require("neoutils.utils.split")
+	end
 end
 
 return M
---
+++ NeoUtils/lua/neoutils/utils/split.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+function M.split(direction)
+	require("telescope.builtin").find_files({
+		attach_mappings = function(_, t_map)
+			t_map("n", "<CR>", function(prompt_bufnr)
+				local selected = require("telescope.actions.state").get_selected_entry()
+				local cmd = direction .. " " .. selected[1]
+				require("telescope.actions").close(prompt_bufnr)
+				vim.api.nvim_command(cmd)
+			end)
+			t_map("i", "<CR>", function(prompt_bufnr)
+				local selected = require("telescope.actions.state").get_selected_entry()
+				local cmd = direction .. " " .. selected[1]
+				require("telescope.actions").close(prompt_bufnr)
+				vim.api.nvim_command(cmd)
+			end)
+			return true
+		end,
+	})
+end
+
+return M
