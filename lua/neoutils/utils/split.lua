08174d55eeff492346eac3cd38620eab3f48c7c2
Nias26 <genangeliludovico26@gmail.com>
2024-10-06 15:39:38 +0200

    refactor(split): changed split handling

--- lua/neoutils/utils/split.lua
+++ lua/neoutils/utils/split.lua
@@ -1,17 +1,21 @@
 local M = {}
 
-function M.split(direction)
+function M.split(opts)
+	opts = opts or {
+		type = "vsplit",
+	}
+
 	require("telescope.builtin").find_files({
 		attach_mappings = function(_, t_map)
 			t_map("n", "<CR>", function(prompt_bufnr)
 				local selected = require("telescope.actions.state").get_selected_entry()
-				local cmd = direction .. " " .. selected[1]
+				local cmd = opts.type .. " " .. selected[1]
 				require("telescope.actions").close(prompt_bufnr)
 				vim.api.nvim_command(cmd)
 			end)
 			t_map("i", "<CR>", function(prompt_bufnr)
 				local selected = require("telescope.actions.state").get_selected_entry()
-				local cmd = direction .. " " .. selected[1]
+				local cmd = opts.type .. " " .. selected[1]
 				require("telescope.actions").close(prompt_bufnr)
 				vim.api.nvim_command(cmd)
 			end)
