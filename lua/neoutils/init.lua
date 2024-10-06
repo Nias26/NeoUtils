8111d0e844d8314e3a1f209b027d9738caaaa462
Nias26 <genangeliludovico26@gmail.com>
2024-10-06 15:34:37 +0200

    refactor: moved dependencies check onto setup func

--- lua/neoutils/init.lua
+++ lua/neoutils/init.lua
@@ -7,6 +7,12 @@ local defaults = {
 }
 
 function M.setup(opts)
+	-- Check for dependencies
+	local status_ok, _ = pcall(require, "telescope")
+	if not status_ok then
+		vim.notify("Error loading telescope", vim.log.levels.ERROR)
+	end
+
 	-- Load options
 	opts = opts or defaults
 
--- plugin/load.lua
+++
@@ -1,5 +0,0 @@
--- Check for dependencies
-local status_ok, _ = pcall(require, "telescope")
-if not status_ok then
-	vim.notify("Error loading telescope", vim.log.levels.ERROR)
-end
